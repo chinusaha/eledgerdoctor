f452ce627a2b6293e7b00ff553d1ccc5db39e035
sahaaditya62 <sahaaditya62@gmail.com>
2018-07-23 23:29:47 +0530

    doctor registration

---
+++ registration-api/README.md
@@ -0,0 +1,2 @@
+# node-mongo-registration-login-api
+
---
+++ registration-api/users/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    username: { type: String, unique: true, required: true },
+    hash: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    licenseID: { type: String, required: true },
+    address: { type: String, required: true },
+    country: { type: String, required: true },
+    province: { type: String, required: true },
+    specialty: { type: String, required: true },
+    contactNumber: { type: String, required: true },
+    createdDate: { type: Date, default: Date.now }
+});
+
+schema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('User', schema);
